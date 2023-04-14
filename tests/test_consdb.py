1c68a038ba3794ded584c295a4ec404b55c1f080
William O'Mullane <womullan@lsst.org>
2023-04-14 16:56:33 -0400

    almost works

--- tests/test_consdb.py
+++ tests/test_consdb.py
@@ -23,7 +23,9 @@ import unittest
 
 
 from lsst.ts import salobj
+from lsst.ts.genericcamera import GenericCameraCsc
 from lsst.consdb import ConsDB, MockCamera
+
 
 STD_TIMEOUT = 15
 SHORT_TIMEOUT = 5
@@ -33,6 +35,7 @@ TEST_CONFIG_DIR = pathlib.Path(__file__).parents[1].\
 
 class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
 
+    the_csc = None
     def make_csc(
         self,
         initial_state=salobj.State.STANDBY,
@@ -41,29 +44,27 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
         **kwargs,
     ):
         print("Called  make csc")
-        return ConsDB(
+        CscTestCase.the_csc = ConsDB(
             initial_state=initial_state,
             config_dir=config_dir,
             simulation_mode=simulation_mode,
             **kwargs,
         )
+        return CscTestCase.the_csc
 
     def basic_make_csc(self, initial_state, config_dir, simulation_mode,
                        override):
         print("Called basic make csc")
         assert simulation_mode == 0
-        return ConsDB(
+        CscTestCase.the_csc =  ConsDB(
             initial_state=initial_state,
             config_dir=config_dir,
             override=override,
         )
-
-    async def asyncSetUp(self) -> None:
-        self.server = self.make_csc()
-        await self.server.start()
+        return CscTestCase.the_csc
 
     async def asyncTearDown(self) -> None:
-        await self.server.stop()
+        pass
 
     async def test_bin_script(self):
         """Test that run_atdometrajectory runs the CSC."""
@@ -74,20 +75,22 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
         )
 
     async def test_configuration(self):
-        mcamera = MockCamera()
+        #mcamera = MockCamera(initial_state=salobj.State.ENABLED)
+        mcamera = GenericCamera(initial_state=salobj.State.ENABLED)
         ConsDB.set_cameras([mcamera.name])
         async with self.make_csc(
             initial_state=salobj.State.STANDBY, config_dir=TEST_CONFIG_DIR
         ):
 
-            cameras = self.server.get_remotes()
+            cameras = CscTestCase.the_csc.get_remotes()
             self.assertGreater(0, len(cameras), "ConsDB did not attach to the camera")
 
     async def test_callback(self):
-        # Need a dummy camer really to send a end of miage metadata message.
+        # Need a dummy camera really to send an end of image metadata message.
         async with self.make_csc(initial_state=salobj.State.STANDBY):
-            await self.remote.cmd_start.start(timeout=STD_TIMEOUT)
-            await self.remote.cmd_enable.start(timeout=STD_TIMEOUT)
+            await CscTestCase.the_csc.disabled_or_enabled()
+        self .assertTrue(CscTestCase.the_csc.summary_state() == salobj.State.ENABLED,
+                         "CSC is not enabled")
         self.assertTrue(False, "Fail for good measure")
 
 
