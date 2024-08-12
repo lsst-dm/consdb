854d857718c6caee0f3d38a5c70df6ebca80387e
Rodrigo Boufleur <rcboufleur@gmail.com>
2024-08-12 11:27:27 -0700

    Fix lint

--- run_transform.sh
+++ run_transform.sh
@@ -1,8 +1,8 @@
 # Example of command used to execute transform_efd
 python python/lsst/consdb/transform_efd.py \
     -i LATISS \
-    -s 2024-04-25T00:00:00  \
-    -e 2024-04-30T23:59:59 \
+    -s 2024-04-30T00:00:00  \
+    -e 2024-04-30T23:59:59.999 \
     -r /repo/embargo \
     -d sqlite:///$PWD/tmp/test.db \
     -E usdf_efd \
