3ae4f7b0771046a94697c9e8beb0f78010fd5d2f
Rodrigo Boufleur <rcboufleur@gmail.com>
2024-08-26 15:31:18 -0300

    Refactor config transformations

--- python/lsst/consdb/efd_transform/summary.py
+++ python/lsst/consdb/efd_transform/summary.py
@@ -1,243 +1,114 @@
-from typing import List, Optional, Union
+from typing import Optional, Union
 
 import numpy
+import pandas
 
 
 class Summary:
+    """Class to summarize and analyze numeric data, handling NaN values and
+    key-based filtering.
     """
-    A class that performs various transformations on a list or numpy
-    array of values.
-    """
 
-    def __init__(self, values: Union[List[Union[float, int, bool, str]], numpy.ndarray]):
+    def __init__(
+        self,
+        dataframe: pandas.DataFrame,
+        datatype: Optional[str] = "object",  # Keep the default datatype to "object"
+    ):
         """
-        Initialize the Transform object.
-
-        Args:
-            values: A list or numpy array of values.
-
-        Returns:
-            None
+        Initialize Summary with a DataFrame of numeric and boolean values.
         """
-        self.values = numpy.array(values) if not isinstance(values, str) else values
 
-    def apply(self, method_name: str) -> Union[float, None]:
+        # Handle NaN values within the DataFrame before conversion
+        dataframe = dataframe.fillna(numpy.nan)  # Ensure NaN values are correctly represented
+
+        # Convert the DataFrame to a NumPy array with the specified datatype
+        self.values = numpy.array(dataframe.to_numpy(), dtype=datatype)
+        self.time = dataframe.index
+
+        # Handle empty or all-NaN arrays after conversion to numpy array
+        if self.values.size == 0 or numpy.all(numpy.isnan(self.values.astype(float))):
+            self.values = numpy.full_like(self.values, numpy.nan, dtype=float)
+
+    def mean(self, **kwargs) -> float:
+        """Calculate the mean ignoring NaN values."""
+        return numpy.nanmean(self.values)
+
+    def stddev(self, ddof: int = 1, **kwargs) -> float:
+        """Calculate the standard deviation ignoring NaN values."""
+        return numpy.nanstd(self.values, ddof=ddof)
+
+    def max(self, **kwargs) -> Union[float, int, bool]:
+        """Find the maximum value ignoring NaN values."""
+        return numpy.nanmax(self.values)
+
+    def min(self, **kwargs) -> Union[float, int, bool]:
+        """Find the minimum value ignoring NaN values."""
+        return numpy.nanmin(self.values)
+
+    def mean_columnwise(self, **kwargs) -> float:
+        """Calculate column-wise mean."""
+        return numpy.apply_along_axis(numpy.nanmean, axis=0, arr=self.values)
+
+    def stddev_columnwise(self, ddof: int = 1, **kwargs) -> float:
+        """Calculate column-wise standard deviation."""
+        return numpy.apply_along_axis(numpy.nanstd, axis=0, arr=self.values, ddof=ddof)
+
+    def max_columnwise(self, **kwargs) -> Union[float, int, bool]:
+        """Find column-wise maximum value."""
+        return numpy.apply_along_axis(numpy.nanmax, axis=0, arr=self.values)
+
+    def min_columnwise(self, **kwargs) -> Union[float, int, bool]:
+        """Find column-wise minimum value."""
+        return numpy.apply_along_axis(numpy.nanmin, axis=0, arr=self.values)
+
+    def rms_from_polynomial_fit(self, degree=1, **kwargs) -> float:
+        """Calculate RMS after fitting a nth-degree polynomial."""
+
+        values = self.values.astype(numpy.float64).flatten()
+        try:
+            index = numpy.isfinite(values)
+            x, y = numpy.arange(len(values))[index], values[index]
+
+            if len(x) <= degree:
+                return numpy.nan
+
+            coeffs = numpy.polyfit(x, y, degree)
+            y_fit = numpy.polyval(coeffs, x)
+            rms_value = numpy.sqrt(numpy.nanmean(y - y_fit) ** 2)
+            return rms_value
+        except Exception as e:
+            print(f"Error occurred during RMS calculation: {str(e)}")
+            return numpy.nan
+
+    def rms_from_polynomial_fit_columnwise(self, degree=1, **kwargs) -> float:
+        """Calculate RMS after fitting a nth-degree polynomial, columnwise."""
+        return numpy.apply_along_axis(
+            self.rms_from_polynomial_fit,
+            axis=0,
+            arr=self.values,
+            **kwargs,
+        )
+
+    def apply(self, method_name: str, **kwargs) -> Union[float, None]:
         """
-        Apply a transformation method specified by method_name.
+        Apply a transformation method specified by method_name with optional
+        kwargs.
 
         Args:
             method_name: Name of the method to apply.
+            **kwargs: Additional keyword arguments.
 
         Returns:
             The result of the transformation method or None if the
             method is not found.
         """
         method = getattr(self, method_name, None)
-        if method:
-            return method()
+        if method and callable(method):
+            try:
+                return method(**kwargs)  # Pass values and kwargs directly to the method
+            except Exception as e:
+                print(f"Error occurred during method '{method_name}' invocation: {str(e)}")
+                return None
         else:
+            print(f"Method '{method_name}' not found or not callable.")
             return None
-
-    def mean(self) -> float:
-        """
-        Calculate the mean of the values.
-
-        Returns:
-            The mean value as a float.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanmean(self.values)
-
-    def col_mean(self) -> float:
-        """
-        Calculate the mean of the values by column.
-
-        Returns:
-            The mean value as a float.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanmean(self.values, axis=0)
-
-    def std(self, ddof: Optional[int] = 1) -> float:
-        """
-        Calculate the standard deviation of the values.
-
-        Args:
-            ddof: Delta degrees of freedom.
-
-        Returns:
-            The standard deviation as a float.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanstd(self.values, ddof=ddof)
-
-    def col_std(self, ddof: Optional[int] = 1) -> float:
-        """
-        Calculate the standard deviation of the values by column.
-
-        Args:
-            ddof: Delta degrees of freedom.
-
-        Returns:
-            The standard deviation as a float.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanstd(self.values, ddof=ddof, axis=0)
-
-    def max(self) -> Union[float, int, bool]:
-        """
-        Find the maximum value in the values.
-
-        Returns:
-            The maximum value as a float, int, or bool.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanmax(self.values)
-
-    def col_max(self) -> Union[float, int, bool]:
-        """
-        Find the maximum value in the values by column.
-
-        Returns:
-            The maximum value as a float, int, or bool.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanmax(self.values, axis=0)
-
-    def min(self) -> Union[float, int, bool]:
-        """
-        Find the minimum value in the values.
-
-        Returns:
-            The minimum value as a float, int, or bool.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanmin(self.values)
-
-    def col_min(self) -> Union[float, int, bool]:
-        """
-        Find the minimum value in the values by column.
-
-        Returns:
-            The minimum value as a float, int, or bool.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.nanmin(self.values, axis=0)
-
-    def logical_and(self) -> Union[bool, numpy.ndarray]:
-        """
-        Perform element-wise logical AND operation on the values.
-
-        Returns:
-            The result of the logical AND operation as a bool or numpy array.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.all(self.values)
-
-    def col_logical_and(self) -> Union[bool, numpy.ndarray]:
-        """
-        Perform element-wise logical AND operation on the values.
-
-        Returns:
-            The result of the logical AND operation as a bool or numpy array.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.all(self.values, axis=0)
-
-    def logical_or(self) -> Union[bool, numpy.ndarray]:
-        """
-        Perform element-wise logical OR operation on the values by column.
-
-        Returns:
-            The result of the logical OR operation as a bool or numpy array.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.any(self.values)
-
-    def col_logical_or(self) -> Union[bool, numpy.ndarray]:
-        """
-        Perform element-wise logical OR operation on the values.
-
-        Returns:
-            The result of the logical OR operation as a bool or numpy array.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return numpy.any(self.values, axis=0)
-
-    def logical_not(self) -> numpy.ndarray:
-        """
-        Perform element-wise logical NOT operation on the values.
-
-        Returns:
-            The result of the logical NOT operation as a numpy array.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return ~numpy.all(self.values)
-
-    def col_logical_not(self) -> numpy.ndarray:
-        """
-        Perform element-wise logical NOT operation on the values by column.
-
-        Returns:
-            The result of the logical NOT operation as a numpy array.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-
-        return ~numpy.all(self.values, axis=0)
-
-    def comma_unique(self) -> str:
-        """
-        Returns a string with unique values separated by commas.
-
-        If the input string is empty, it returns NaN.
-
-        Returns:
-            str: A string with unique values separated by commas.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-        values = self.values.split(",")
-        return ",".join(numpy.unique(values))
-
-    def semicolon_unique(self) -> str:
-        """
-        Returns a string with semicolon-separated unique values.
-
-        If the input string is empty, it returns NaN.
-        This method splits the input string by semicolons and returns a
-        new string with only the unique values, separated by semicolons.
-
-        Returns:
-            str: A string with semicolon-separated unique values.
-        """
-        if numpy.size(self.values) == 0:
-            return numpy.nan
-        values = self.values.split(";")
-        return ";".join(numpy.unique(values))
