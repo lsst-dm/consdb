2c42dff88b7fd79f530cf1e0470a13332a377168
Brian Brondel <BBrondel@lsst.org>
2024-09-25 08:20:58 -0700

    Fix bug in recursive directory processing

--- python/lsst/consdb/hinfo.py
+++ python/lsst/consdb/hinfo.py
@@ -411,10 +411,14 @@ def process_local_path(path: str) -> None:
         Path to directory that contains yaml files.
     """
     if os.path.isdir(path):
-        for _, _, files in os.walk(path):
+        for root, _, files in os.walk(path):
             for file in files:
                 if file.endswith(".yaml"):
-                    process_resource(ResourcePath(file), get_instrument_dict(instrument))
+                    try:
+                        logger.info(f"Processing: {file}...")
+                        process_resource(ResourcePath(os.path.join(root, file)), get_instrument_dict(instrument))
+                    except:
+                        logger.exception(f"Failed to process resource {file}")
     # If a yaml file is provided on the command line, process it.
     elif os.path.isfile(path) and path.endswith(".yaml"):
         process_resource(ResourcePath(path), get_instrument_dict(instrument))
