7ca7d078e85160fb966695cfc2cf4200a720aa6f
Rodrigo Boufleur <rcboufleur@gmail.com>
2024-10-24 10:49:57 -0700

    Update temporary files

--- tests/efd_transform/run_transform_efd.sh
+++ tests/efd_transform/run_transform_efd.sh
@@ -1,21 +1,21 @@
 # Example of command used to execute transform_efd
 # Considering the command is being executed at the root of the consdb repository
 
-python python/lsst/consdb/efd_transform/transform_efd.py \
-    -i LATISS \
-    -r s3://rubin-summit-users/butler.yaml \
-    -d sqlite:///$PWD/tests/efd_transform/test.db \
-    -E usdf_efd \
-    -c python/lsst/consdb/efd_transform/config_LATISS.yaml \
-    -t 60 \
-    -l $PWD/tmp/transform.log
-
 # python python/lsst/consdb/efd_transform/transform_efd.py \
 #     -i LATISS \
-#     -s 2024-04-24T23:00:00  \
-#     -e 2024-04-25T00:00:00 \
 #     -r s3://rubin-summit-users/butler.yaml \
 #     -d sqlite:///$PWD/tests/efd_transform/test.db \
 #     -E usdf_efd \
 #     -c python/lsst/consdb/efd_transform/config_LATISS.yaml \
+#     -t 60 \
 #     -l $PWD/tmp/transform.log
+
+python python/lsst/consdb/efd_transform/transform_efd.py \
+    -i LATISS \
+    -s 2024-04-25T00:00:00  \
+    -e 2024-04-25T00:05:00 \
+    -r s3://rubin-summit-users/butler.yaml \
+    -d sqlite:///$PWD/tests/efd_transform/test.db \
+    -E usdf_efd \
+    -c python/lsst/consdb/efd_transform/config_LATISS.yaml \
+    -l $PWD/tmp/transform.log
