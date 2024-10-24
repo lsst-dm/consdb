9b7afeffa194eb199c9a36585036bbee47829b63
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2024-10-24 11:46:16 -0700

    Add queue manager for periodic execution control

--- tests/efd_transform/run_transform_efd.sh
+++ tests/efd_transform/run_transform_efd.sh
@@ -1,21 +1,24 @@
 # Example of command used to execute transform_efd
 # Considering the command is being executed at the root of the consdb repository
 
+# Real-time mode proccess 5 minuts of data
+python python/lsst/consdb/efd_transform/transform_efd.py \
+    -i LATISS \
+    -r s3://rubin-summit-users/butler.yaml \
+    -d sqlite:///$PWD/tests/efd_transform/test.db \
+    -E usdf_efd \
+    -c python/lsst/consdb/efd_transform/config_LATISS.yaml \
+    -t 5 \
+    -w 1 \
+    -l $PWD/tmp/transform.log
+
+# Processing fixed period of data in chunks of 20 minutes
 # python python/lsst/consdb/efd_transform/transform_efd.py \
 #     -i LATISS \
 #     -r s3://rubin-summit-users/butler.yaml \
 #     -d sqlite:///$PWD/tests/efd_transform/test.db \
 #     -E usdf_efd \
 #     -c python/lsst/consdb/efd_transform/config_LATISS.yaml \
-#     -t 60 \
+#     -t 20 \
+#     -w 1 \
 #     -l $PWD/tmp/transform.log
-
-python python/lsst/consdb/efd_transform/transform_efd.py \
-    -i LATISS \
-    -s 2024-04-25T00:00:00  \
-    -e 2024-04-25T00:05:00 \
-    -r s3://rubin-summit-users/butler.yaml \
-    -d sqlite:///$PWD/tests/efd_transform/test.db \
-    -E usdf_efd \
-    -c python/lsst/consdb/efd_transform/config_LATISS.yaml \
-    -l $PWD/tmp/transform.log
