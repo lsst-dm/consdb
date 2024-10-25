14e10e64c4fc827d6a3d8a5cd960e4c38b0cf88d
Rodrigo Boufleur <rcboufleur@gmail.com>
2024-10-25 09:38:31 -0700

    Update schema structures

--- tests/efd_transform/run_transform_efd.sh
+++ tests/efd_transform/run_transform_efd.sh
@@ -4,7 +4,7 @@
 # Real-time mode proccess 5 minuts of data
 python python/lsst/consdb/efd_transform/transform_efd.py \
     -i LATISS \
-    -r /repo/embargo_old \
+    -r s3://rubin-summit-users/butler.yaml \
     -d sqlite:///$PWD/tests/efd_transform/LATISS.db \
     -E usdf_efd \
     -c python/lsst/consdb/efd_transform/config_LATISS.yml \
@@ -33,6 +33,7 @@ python python/lsst/consdb/efd_transform/transform_efd.py \
     -t 5 \
     -w 1 \
     -l $PWD/tmp/transform.log
+
 # Processing fixed period of data in chunks of 20 minutes
 # python python/lsst/consdb/efd_transform/transform_efd.py \
 #     -i LATISS \
@@ -41,5 +42,44 @@ python python/lsst/consdb/efd_transform/transform_efd.py \
 #     -E usdf_efd \
 #     -c python/lsst/consdb/efd_transform/config_LATISS.yaml \
 #     -t 20 \
+#     -w 1 \
+#     -l $PWD/tmp/transform.log
+
+
+# python python/lsst/consdb/efd_transform/transform_efd.py \
+#     -i LATISS \
+#     -s 2024-10-17T12:00:00 \
+#     -e 2024-10-18T12:00:00 \
+#     -r s3://rubin-summit-users/butler.yaml \
+#     -d sqlite:///$PWD/tests/efd_transform/LATISS.db \
+#     -E usdf_efd \
+#     -c python/lsst/consdb/efd_transform/config_LATISS.yml \
+#     -t 60 \
+#     -w 1 \
+#     -l $PWD/tmp/transform.log
+
+
+# python python/lsst/consdb/efd_transform/transform_efd.py \
+#     -i LSSTComCam \
+#     -s 2024-10-17T12:00:00 \
+#     -e 2024-10-18T12:00:00 \
+#     -r /repo/embargo_new \
+#     -d sqlite:///$PWD/tests/efd_transform/LSSTComCam.db \
+#     -E usdf_efd \
+#     -c python/lsst/consdb/efd_transform/config_LSSTComCam.yml \
+#     -t 5 \
+#     -w 1 \
+#     -l $PWD/tmp/transform.log
+
+
+# python python/lsst/consdb/efd_transform/transform_efd.py \
+#     -i LSSTComCamSim \
+#     -s 2024-10-17T12:00:00 \
+#     -e 2024-10-18T12:00:00 \
+#     -r /repo/embargo_new \
+#     -d sqlite:///$PWD/tests/efd_transform/LSSTComCamSim.db \
+#     -E usdf_efd \
+#     -c python/lsst/consdb/efd_transform/config_LSSTComCamSim.yml \
+#     -t 5 \
 #     -w 1 \
 #     -l $PWD/tmp/transform.log
