09545268af4587557afbf0dba7902d214bbdeecf
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2024-11-06 10:47:16 -0800

    Update bash run script

--- tests/efd_transform/run_transform_efd.sh
+++ tests/efd_transform/run_transform_efd.sh
@@ -2,37 +2,37 @@
 # Considering the command is being executed at the root of the consdb repository
 
 # Real-time mode proccess 5 minuts of data
-python python/lsst/consdb/efd_transform/transform_efd.py \
-    -i LATISS \
-    -r s3://rubin-summit-users/butler.yaml \
-    -d sqlite:///$PWD/tests/efd_transform/LATISS.db \
-    -E usdf_efd \
-    -c python/lsst/consdb/efd_transform/config_LATISS.yml \
-    -t 5 \
-    -w 1 \
-    -l $PWD/tmp/transform.log
+# python python/lsst/consdb/efd_transform/transform_efd.py \
+#     -i LATISS \
+#     -r s3://rubin-summit-users/butler.yaml \
+#     -d sqlite:///$PWD/tests/efd_transform/LATISS.db \
+#     -E usdf_efd \
+#     -c python/lsst/consdb/efd_transform/config_LATISS.yml \
+#     -t 5 \
+#     -w 1 \
+#     -l $PWD/tmp/transform.log
 
 
-python python/lsst/consdb/efd_transform/transform_efd.py \
-    -i LSSTComCam \
-    -r /repo/embargo_new \
-    -d sqlite:///$PWD/tests/efd_transform/LSSTComCam.db \
-    -E usdf_efd \
-    -c python/lsst/consdb/efd_transform/config_LSSTComCam.yml \
-    -t 5 \
-    -w 1 \
-    -l $PWD/tmp/transform.log
+# python python/lsst/consdb/efd_transform/transform_efd.py \
+#     -i LSSTComCam \
+#     -r /repo/embargo_new \
+#     -d sqlite:///$PWD/tests/efd_transform/LSSTComCam.db \
+#     -E usdf_efd \
+#     -c python/lsst/consdb/efd_transform/config_LSSTComCam.yml \
+#     -t 5 \
+#     -w 1 \
+#     -l $PWD/tmp/transform.log
 
 
-python python/lsst/consdb/efd_transform/transform_efd.py \
-    -i LSSTComCamSim \
-    -r /repo/embargo_new \
-    -d sqlite:///$PWD/tests/efd_transform/LSSTComCamSim.db \
-    -E usdf_efd \
-    -c python/lsst/consdb/efd_transform/config_LSSTComCamSim.yml \
-    -t 5 \
-    -w 1 \
-    -l $PWD/tmp/transform.log
+# python python/lsst/consdb/efd_transform/transform_efd.py \
+#     -i LSSTComCamSim \
+#     -r /repo/embargo_new \
+#     -d sqlite:///$PWD/tests/efd_transform/LSSTComCamSim.db \
+#     -E usdf_efd \
+#     -c python/lsst/consdb/efd_transform/config_LSSTComCamSim.yml \
+#     -t 5 \
+#     -w 1 \
+#     -l $PWD/tmp/transform.log
 
 # Processing fixed period of data in chunks of 20 minutes
 # python python/lsst/consdb/efd_transform/transform_efd.py \
@@ -72,14 +72,14 @@ python python/lsst/consdb/efd_transform/transform_efd.py \
 #     -l $PWD/tmp/transform.log
 
 
-# python python/lsst/consdb/efd_transform/transform_efd.py \
-#     -i LSSTComCamSim \
-#     -s 2024-10-17T12:00:00 \
-#     -e 2024-10-18T12:00:00 \
-#     -r /repo/embargo_new \
-#     -d sqlite:///$PWD/tests/efd_transform/LSSTComCamSim.db \
-#     -E usdf_efd \
-#     -c python/lsst/consdb/efd_transform/config_LSSTComCamSim.yml \
-#     -t 5 \
-#     -w 1 \
-#     -l $PWD/tmp/transform.log
+python python/lsst/consdb/efd_transform/transform_efd.py \
+    -i LSSTComCamSim \
+    -r /repo/embargo_new \
+    -d sqlite:///$PWD/tests/efd_transform/LSSTComCamSim.db \
+    -E usdf_efd \
+    -c python/lsst/consdb/efd_transform/config_LSSTComCamSim.yml \
+    -t 5 \
+    -w 1 \
+    -l $PWD/tmp/transform.log
+    # -s 2024-10-17T12:00:00 \
+    # -e 2024-10-18T12:00:00 \
