4cb629d681e46e127798e7bd827e40aa71f71a75
Rodrigo Boufleur <rcboufleur@gmail.com>
2024-12-08 02:58:14 -0800

    Fix pytest for config_model

--- tests/efd_transform/test_config_model.py
+++ tests/efd_transform/test_config_model.py
@@ -1,135 +1,56 @@
 import pytest
 from pydantic import ValidationError
 
-from python.lsst.consdb.efd_transform.config_model import Column, ConfigModel, Field, Topic
+from python.lsst.consdb.efd_transform.config_model import UNPIVOTED_TABLES, Column, ConfigModel, Field, Topic
 
 
-def get_columns():
-    columns = [
+def test_column_creation_valid():
+    topic = Topic(name="topic1", fields=[Field(name="field1")])
+    column = Column(
+        name="valid_column",
+        tables=UNPIVOTED_TABLES,
+        store_unpivoted=True,
+        function="mean",
+        datatype="float",
+        unit="kg",
+        description="A valid column",
+        packed_series=False,
+        topics=[topic],
+    )
+    assert column.name == "valid_column"
+    assert column.tables == UNPIVOTED_TABLES
+
+
+def test_column_with_invalid_tables():
+    topic = Topic(name="topic2", fields=[Field(name="field2")])
+    with pytest.raises(ValidationError) as excinfo:
         Column(
-            name="column1",
-            function="function1",
-            type="type1",
-            unit="unit1",
-            description="description1",
-            topics=[
-                Topic(
-                    name="topic1",
-                    fields=[
-                        Field(name="field1", is_array=False),
-                        Field(name="field2", is_array=True),
-                    ],
-                ),
-                Topic(
-                    name="topic2",
-                    fields=[
-                        Field(name="field3", is_array=False),
-                        Field(name="field4", is_array=True),
-                    ],
-                ),
-            ],
-        ),
-        Column(
-            name="column2",
-            function="function2",
-            type="type2",
-            unit="unit2",
-            description="description2",
-            topics=[
-                Topic(
-                    name="topic3",
-                    fields=[
-                        Field(name="field5", is_array=False),
-                        Field(name="field6", is_array=True),
-                    ],
-                ),
-            ],
-        ),
-    ]
-
-    return columns
+            name="invalid_column",
+            tables=["invalid_table"],
+            store_unpivoted=True,
+            function="sum",
+            datatype="float",
+            unit="m",
+            description="An invalid column",
+            packed_series=False,
+            topics=[topic],
+        )
+    assert "Invalid tables provided" in str(excinfo.value)
 
 
-def test_config_model_valid():
-    columns = get_columns()
-
-    config_model = ConfigModel(columns=columns)
-
-    assert config_model.columns == columns
-
-
-def test_config_model_invalid():
-
-    # Missing required attribute 'columns'
-    with pytest.raises(ValidationError):
-        ConfigModel()
-
-    # Invalid type for 'columns'
-    with pytest.raises(ValidationError):
-        ConfigModel(columns="invalid")
-
-    # Invalid type for 'name' in Column
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].name = 123
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'function' in Column
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].function = 123
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'type' in Column
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].type = 123
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'unit' in Column
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].unit = 123
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'description' in Column
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].description = 123
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'tables' in Column
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].tables = "invalid"
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'topics' in Column
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].topics = "invalid"
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'name' in Topic
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].topics[0].name = 123
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'fields' in Topic
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].topics[0].fields = "invalid"
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'name' in Field
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].topics[0].fields[0].name = 123
-        ConfigModel.model_validate(columns)
-
-    # Invalid type for 'is_array' in Field
-    with pytest.raises(ValidationError):
-        columns = get_columns()
-        columns[0].topics[0].fields[0].is_array = "invalid"
-        ConfigModel.model_validate(columns)
+def test_config_model_creation():
+    topic = Topic(name="topic3", fields=[Field(name="field3")])
+    column = Column(
+        name="column_in_model",
+        tables=UNPIVOTED_TABLES,
+        store_unpivoted=True,
+        function="max",
+        datatype="int",
+        unit="s",
+        description="A column within a model",
+        packed_series=True,
+        topics=[topic],
+    )
+    config_model = ConfigModel(columns=[column])
+    assert len(config_model.columns) == 1
+    assert config_model.columns[0].name == "column_in_model"
