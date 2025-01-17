b6bc6d0a54bd732fe0ce0df4073567b11ca5c356
Rodrigo Boufleur <rcboufleur@gmail.com>
2025-01-17 06:34:32 -0800

    Fix linting issues across the codebase

--- python/lsst/consdb/efd_transform/dao/butler.py
+++ python/lsst/consdb/efd_transform/dao/butler.py
@@ -1,43 +1,59 @@
+"""Provides the `ButlerDao` class for interacting with data via a Butler object.
+
+It includes methods to query dimensions and retrieve exposures or visits within
+specified time periods.
+"""
+
 import astropy.time
 import pandas
 from lsst.daf.butler import Butler
 
 
 class ButlerDao:
-    """
-    A class that provides data access methods for querying dimensions using
-    a Butler object.
+    """A class that provides data access methods for querying Butler.
 
     Args:
+    ----
         butler (Butler): The Butler object used for data access.
 
     """
 
     def __init__(self, butler: Butler):
+        """Initialize the `ButlerDao` class.
+
+        Args:
+        ----
+            butler (Butler): The Butler object used for data access.
+
+        """
         self.butler = butler
 
     def query_dimension_to_list(self, resultset) -> list:
-        """
-        Converts the given resultset to a list of dictionaries.
+        """Convert given resultset to a list of dictionaries.
 
         Args:
+        ----
             resultset: The resultset to be converted.
 
         Returns:
+        -------
             A list of dictionaries representing the resultset.
+
         """
         list_of_dicts = [r.toDict() for r in resultset]
         return list_of_dicts
 
     def query_dimension_to_dataframe(self, resultset) -> pandas.DataFrame:
-        """
-        Converts a resultset of query dimensions to a pandas DataFrame.
+        """Convert resultset of query dimensions to a pandas DataFrame.
 
         Args:
+        ----
             resultset: The resultset of query dimensions.
 
         Returns:
+        -------
             A pandas DataFrame containing the query dimensions.
+
         """
         return pandas.DataFrame(self.query_dimension_to_list(resultset))
 
@@ -47,21 +63,22 @@ class ButlerDao:
         start_time: astropy.time.Time,
         end_time: astropy.time.Time,
     ) -> list:
-        """
-        Retrieve a list of exposures within a specified time period for a
-        given instrument.
+        """Retrieve list of exposures givem time period for a specific instrument.
 
         Args:
+        ----
             instrument (str): The instrument name.
             start_time (astropy.time.Time): The start time of the period.
             end_time (astropy.time.Time): The end time of the period.
 
         Returns:
+        -------
             list: A list of exposures within the specified time period.
 
         """
         where_clause = (
-            f"instrument=instr and exposure.timespan OVERLAPS (T'{start_time}/utc', T'{end_time}/utc')"
+            f"instrument=instr and exposure.timespan OVERLAPS "
+            f"(T'{start_time}/utc', T'{end_time}/utc')"
         )
 
         resultset = self.butler.registry.queryDimensionRecords(
@@ -75,21 +92,22 @@ class ButlerDao:
         start_time: astropy.time.Time,
         end_time: astropy.time.Time,
     ) -> list:
-        """
-        Retrieve a list of visits within a specified time period for a given
-        instrument.
+        """Retrieve list of visits for specified time period given an instrument.
 
         Args:
+        ----
             instrument (str): The instrument name.
             start_time (astropy.time.Time): The start time of the period.
             end_time (astropy.time.Time): The end time of the period.
 
         Returns:
+        -------
             list: A list of visits within the specified time period.
 
         """
         where_clause = (
-            f"instrument=instr and visit.timespan OVERLAPS (T'{start_time}/utc', T'{end_time}/utc')"
+            f"instrument=instr and visit.timespan OVERLAPS "
+            f"(T'{start_time}/utc', T'{end_time}/utc')"
         )
 
         resultset = self.butler.registry.queryDimensionRecords(
--- python/lsst/consdb/efd_transform/dao/visit_efd.py
+++ python/lsst/consdb/efd_transform/dao/visit_efd.py
@@ -1,33 +1,45 @@
+"""Defines the `VisitEfdDao` class."""
+
 import pandas
 from lsst.consdb.efd_transform.dao.base import DBBase
 from sqlalchemy.sql import and_, select
 
 
 class VisitEfdDao(DBBase):
-    """
-    Data Access Object for visit1_efd table.
+    """Data Access Object for visit1_efd table.
 
     Args:
+    ----
         db_uri (str): The URI of the database.
 
     Attributes:
+    ----------
         tbl: The visit1_efd table object.
 
     """
 
     def __init__(self, db_uri: str, schema: str):
+        """Initialize the `VisitEfdDao` class.
+
+        Args:
+        ----
+            db_uri (str): The URI of the database.
+            schema (str): The schema name in the database.
+
+        """
         super(VisitEfdDao, self).__init__(db_uri, schema)
 
         self.tbl = self.get_table("visit1_efd", schema=schema)
 
     def get_by_visit_id(self, visit_id: int):
-        """
-        Retrieve rows from the table based on visit_id.
+        """Retrieve rows from the table based on visit_id.
 
         Args:
+        ----
             visit_id (int): The visit_id to filter the rows.
 
         Returns:
+        -------
             list: A list of rows matching the visit_id.
 
         """
@@ -38,14 +50,15 @@ class VisitEfdDao(DBBase):
         return rows
 
     def get_by_visit_id_instrument(self, visit_id: int, instrument: str):
-        """
-        Retrieve rows from the table based on visit_id and instrument.
+        """Retrieve rows from the table based on visit_id and instrument.
 
         Args:
+        ----
             visit_id (int): The visit_id to filter the rows.
             instrument (str): The instrument to filter the rows.
 
         Returns:
+        -------
             list: A list of rows matching the visit_id and instrument.
 
         """
@@ -58,25 +71,26 @@ class VisitEfdDao(DBBase):
         return rows
 
     def count(self):
-        """
-        Get the count of rows in the table.
+        """Get the count of rows in the table.
 
-        Returns:
+        Returns
+        -------
             int: The count of rows in the table.
 
         """
         return self.execute_count(self.tbl)
 
     def upsert(self, df: pandas.DataFrame, commit_every: int = 100) -> int:
-        """
-        Upsert data into the table.
+        """Upsert data into the table.
 
         Args:
+        ----
             df (pandas.DataFrame): The DataFrame containing the data to upsert.
             commit_every (int, optional): The number of rows to commit at once.
             Defaults to 100.
 
         Returns:
+        -------
             int: The number of rows upserted.
 
         """
