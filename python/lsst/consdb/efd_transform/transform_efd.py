e7abad1e75b03efbf3fe0acc1bc92c94ec113e19
Rodrigo Boufleur <rcboufleur@gmail.com>
2025-01-27 09:21:32 -0800

    Refactor and update jobs and cronjobs workflow

--- python/lsst/consdb/efd_transform/transform_efd.py
+++ python/lsst/consdb/efd_transform/transform_efd.py
@@ -8,22 +8,18 @@ import argparse
 import asyncio
 import logging
 import sys
-from datetime import datetime, timezone
+from datetime import datetime
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
-import astropy.time
-
-# import lsst_efd_client
 import yaml
+from astropy.time import Time, TimeDelta
 from lsst.consdb.efd_transform.config_model import ConfigModel
 from lsst.consdb.efd_transform.dao.influxdb import InfluxDbDao
 from lsst.consdb.efd_transform.queue_manager import QueueManager
 from lsst.consdb.efd_transform.transform import Transform
 from lsst.daf.butler import Butler
 from pydantic import ValidationError
-
-# from sqlalchemy import create_engine
 
 
 def get_logger(path: str, debug: bool = True) -> logging.Logger:
@@ -38,15 +34,12 @@ def get_logger(path: str, debug: bool = True) -> logging.Logger:
     Returns:
     -------
         logging.Logger: The configured logger object.
-
     """
-    # File Handler
     logfile = Path(path)
     formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
     file_handler = logging.FileHandler(logfile)
     file_handler.setFormatter(formatter)
 
-    # Stdout handler
     consoleFormatter = logging.Formatter("[%(levelname)s] %(message)s")
     consoleHandler = logging.StreamHandler(sys.stdout)
     consoleHandler.setFormatter(consoleFormatter)
@@ -69,30 +62,15 @@ def build_argparser() -> argparse.ArgumentParser:
     Returns
     -------
         argparse.ArgumentParser: The argument parser object.
-
     """
     parser = argparse.ArgumentParser(description="Summarize EFD topics in a time range")
     parser.add_argument("-c", "--config", dest="config_name", required=True, help="config YAML")
+    parser.add_argument("-i", "--instrument", dest="instrument", required=True, help="instrument name")
     parser.add_argument(
-        "-i",
-        "--instrument",
-        dest="instrument",
-        required=True,
-        help="instrument name",
+        "-s", "--start", dest="start_time", required=False, help="start time (ISO, YYYY-MM-DDTHH:MM:SS)"
     )
     parser.add_argument(
-        "-s",
-        "--start",
-        dest="start_time",
-        required=False,
-        help="start time (ISO, YYYY-MM-DDTHH:MM:SS)",
-    )
-    parser.add_argument(
-        "-e",
-        "--end",
-        dest="end_time",
-        required=False,
-        help="end time (ISO, YYYY-MM-DDTHH:MM:SS)",
+        "-e", "--end", dest="end_time", required=False, help="end time (ISO, YYYY-MM-DDTHH:MM:SS)"
     )
     parser.add_argument(
         "-r",
@@ -103,21 +81,9 @@ def build_argparser() -> argparse.ArgumentParser:
         help="Butler repo",
     )
     parser.add_argument(
-        "-d",
-        "--db",
-        dest="db_conn_str",
-        # default="sqlite://test.db",
-        required=True,
-        help="Consolidated Database connection string",
+        "-d", "--db", dest="db_conn_str", required=True, help="Consolidated Database connection string"
     )
-    parser.add_argument(
-        "-E",
-        "--efd",
-        dest="efd_conn_str",
-        # default="usdf_efd",
-        required=True,
-        help="EFD connection string",
-    )
+    parser.add_argument("-E", "--efd", dest="efd_conn_str", required=True, help="EFD connection string")
     parser.add_argument(
         "-t",
         "--timedelta",
@@ -132,16 +98,26 @@ def build_argparser() -> argparse.ArgumentParser:
         dest="timewindow",
         default=1,
         required=False,
-        help="Processing overlaping time window in minutes",
+        help="Processing overlapping time window in minutes",
+    )
+    parser.add_argument("-l", "--logfile", dest="logfile", default="transform.log", help="Log file")
+    parser.add_argument(
+        "-m",
+        "--mode",
+        dest="mode",
+        choices=["job", "cronjob"],
+        required=True,
+        help="Execution mode: 'job' for Kubernetes Job or 'cronjob' for periodic tasks",
     )
     parser.add_argument(
-        "-l",
-        "--logfile",
-        dest="logfile",
-        default="transform.log",
-        help="Log file",
+        "-R",
+        "--resume",
+        dest="resume",
+        action="store_true",
+        default=False,
+        required=False,
+        help="Resume idle tasks processing",
     )
-
     return parser
 
 
@@ -159,215 +135,247 @@ def read_config(config_name: str) -> Dict[str, Any]:
     Raises:
     ------
         ValidationError: If the configuration file is invalid.
-
     """
     try:
         with open(config_name, "r") as file:
             data = yaml.safe_load(file)
             config = ConfigModel(**data)
-
             return config.model_dump()
-
     except ValidationError as e:
         raise e
 
 
-async def main() -> None:
-    """Entry point of the program.
+def _to_astropy_time(dt: Optional[datetime]) -> Optional[Time]:
+    """Convert a datetime object to an Astropy Time object.
 
-    This function performs the main logic of the program, including parsing
-    command line arguments, setting up logging, creating necessary objects,
-    and processing the specified time interval.
+    Args:
+    ----
+        dt (Optional[datetime]): The datetime object to convert.
+
+    Returns:
+    -------
+        Optional[Time]: The converted Astropy Time object, or None if dt is None.
     """
-    t0 = datetime.now()
-    parser = build_argparser()
+    if dt is None:
+        return None
+    return Time(dt.isoformat(), format="isot", scale="utc")
 
-    args = parser.parse_args()
 
-    log = get_logger(args.logfile)
+def _process_task(
+    task: Dict,
+    qm: QueueManager,
+    tm: Transform,
+    log: logging.Logger,
+    instrument: str,
+    timewindow: int,
+    retry: bool = False,
+) -> Dict[str, int]:
+    """Process a single task and update its status.
 
-    # TODO: Remove this or make it optional
-    log.debug("==============================================================")
-    log.debug("Testing postgres connection")
-    # ConsDB DB URI
-    consdb_url = args.db_conn_str
+    Args:
+        task (Dict): The task to process.
+        qm (QueueManager): The queue manager instance.
+        tm (Transform): The transform instance.
+        log (logging.Logger): The logger instance.
+        instrument (str): The instrument name.
+        timewindow (int): The time window value.
+
+    Returns:
+        Dict[str, int]: A dictionary containing the counts of exposures and visits.
+    """
+    log.info("----------------------------------------------------------")
+    log.info(f"Task ID: {task['id']}")
+    log.info(f"Start Time: {task['start_time']} End Time: {task['end_time']}")
+    log.info(f"Duration: {task['end_time'] - task['start_time']}")
+
+    qm.dao.task_started(task["id"])
     try:
-        from dao.base import DBBase
-        from dao.exposure_efd import ExposureEfdDao
-        from dao.visit_efd import VisitEfdDao
+        process_count = tm.process_interval(
+            instrument,
+            start_time=_to_astropy_time(task["start_time"]) - TimeDelta(timewindow * 60, format="sec"),
+            end_time=_to_astropy_time(task["end_time"]) + TimeDelta(timewindow * 60, format="sec"),
+        )
 
-        consdb = DBBase(consdb_url)
-        log.debug(f"ConsDB engine: {consdb.get_db_engine()}")
-        log.debug(f"ConsDB connection: {consdb.get_con()}")
-        log.debug("Postgres connection successful")
+        qm.dao.task_update_counts(
+            task["id"],
+            exposures=process_count["exposures"],
+            visits1=process_count["visits1"],
+        )
 
-        log.debug("Testing LATISS table exposure_efd")
-        expdao = ExposureEfdDao(consdb_url, "cdb_latiss")
-        log.debug(f"exposure_efd table: {expdao.tbl}")
-
-        log.debug("Testing LATISS table visit1_efd")
-        visdao = VisitEfdDao(consdb_url, "cdb_latiss")
-        log.debug(f"visit1_efd table: {visdao.tbl}")
+        qm.dao.task_completed(task["id"])
+        if retry:
+            qm.dao.task_retries_increment(task["id"])
+        return process_count
 
     except Exception as e:
-        log.error(f"Postgres connection failed: {e}")
-        sys.exit(1)
-    log.debug("==============================================================")
+        log.error(f"Error processing task {task['id']}: {e}")
+        qm.dao.task_failed(task["id"], error=str(e))
+        return {"exposures": 0, "visits1": 0}
 
-    # Instantiate the butler
-    butler = Butler(args.repo)
-    log.debug(f"Butler: {butler}")
 
-    # Instantiate the EFD
-    efd = InfluxDbDao(args.efd_conn_str)
-    log.debug(f"EFD: {efd}")
+async def handle_job(qm: QueueManager, log: logging.Logger, args, start_time, end_time):
+    """Handle tasks for Kubernetes job workflow.
 
-    config = read_config(args.config_name)
-    log.debug(f"Configs Columns: {len(config['columns'])}")
-
-    # TODO: Commit every can be a enviroment variable
-    commit_every = 100
-
-    # Instantiate the main class transform
-    tm = Transform(
-        butler=butler,
-        db_uri=consdb_url,
-        efd=efd,
-        config=config,
-        logger=log,
-        commit_every=commit_every,
-    )
-
-    # Instantiate the queue manager
-    qm = QueueManager(
-        db_uri=consdb_url,
-        schema=tm.get_schema_by_instrument(args.instrument),
-        logger=log,
-    )
-
-    start_time = None
-    if args.start_time is not None:
-        start_time = datetime.fromisoformat(args.start_time)
-        start_time = astropy.time.Time(start_time.isoformat(), format="isot", scale="utc")
-
-    end_time = None
-    if args.end_time is not None:
-        end_time = datetime.fromisoformat(args.end_time)
-        end_time = astropy.time.Time(end_time.isoformat(), format="isot", scale="utc")
-
-    # Run the transform script with fixed interval
-    # TODO: Translate this
-    # Execution for a specific period or reprocessing.
-    # creates new tasks and executes only the created tasks.
-    # Does not consider existing tasks as pending.
-    if start_time is not None and end_time is not None:
-        log.info("Running the transform script with fixed interval")
+    Args:
+        qm (QueueManager): The queue manager instance.
+        log (logging.Logger): The logger instance.
+        args: Parsed arguments.
+        start_time: Start time as Astropy Time.
+        end_time: End time as Astropy Time.
+    """
+    print(args)
+    if args.resume:
+        log.info("Resuming queued tasks.")
+        tasks = qm.waiting_tasks(args.repo, "idle")
+    else:
+        log.info("Creating tasks for the fixed interval.")
         tasks = qm.create_tasks(
             start_time=start_time,
             end_time=end_time,
             process_interval=int(args.timedelta),
             time_window=int(args.timewindow),
             status="idle",
+            butler_repo=args.repo,
         )
 
-    else:
-        log.info("Running the transform script with periodic interval")
-        # Periodic cronjob execution.
-        # Check the most recent pending tasks (end_time desc)
-        # If it does not find any, it will create new tasks using the
-        # last execution.
-        tasks = qm.recent_tasks_to_run()
+    # check for failed tasks and add them to the processing queue again
+    failed_tasks = qm.failed_tasks(args.repo, max_retries=3)
 
-        if len(tasks) == 0:
-            log.debug("No tasks found.")
-            # The created tasks cannot be executed now because the end_time is
-            # greater than the current time (now + time window).
-            # In the next iteration they will be executed
-            qm.create_tasks(
-                process_interval=int(args.timedelta),
-                time_window=int(args.timewindow),
-            )
+    tasks += failed_tasks
+    return tasks
 
-            # All tasks with pending status ordered by end time desc
-            # As long as end_time is less than the current time
-            tasks = qm.recent_tasks_to_run()
 
-    count_task = 0
-    # Count exposures and visits1 includind overlaps between tasks
-    count_exposures = 0
-    count_visits1 = 0
+async def handle_cronjob(qm: QueueManager, log: logging.Logger, args):
+    """Handle tasks for CronJob workflow.
 
-    # TODO: Perhaps it would be interesting to test the end_time of each task
-    # and if it is less than now, create a new task until it reaches
-    # the current time.
-    for task in tasks:
-        log.info("----------------------------------------------------------")
-        log.info(f"Next Task: ID: {task['id']}")
-        log.info(
-            f"Current Time: "
-            f"{datetime.now(tz=timezone.utc).replace(tzinfo=None).isoformat(timespec='seconds')}"
+    Args:
+        qm (QueueManager): The queue manager instance.
+        log (logging.Logger): The logger instance.
+        args: Parsed arguments.
+    """
+    log.info("Retrieving tasks for periodic interval.")
+
+    # negative margin means the amount of time that must have passed in seconds
+    # since end_time, it ensures running tasks with time intervals in the past
+    tasks = qm.recent_tasks_to_run(margin_seconds=-60)
+
+    # check if there are queued pending tasks
+    waiting_tasks = qm.waiting_tasks(args.repo, "pending")
+
+    if not tasks and not waiting_tasks:
+        log.info("No tasks found. Creating new tasks.")
+        qm.create_tasks(
+            start_time=None,
+            end_time=None,
+            process_interval=int(args.timedelta),
+            time_window=int(args.timewindow),
+            butler_repo=args.repo,
         )
-        log.info(
-            f"Task Start: {task['start_time']} End: {task['end_time']} = "
-            f"{task['end_time'] - task['start_time']}"
-        )
+        tasks = qm.recent_tasks_to_run(margin_seconds=-60)
 
-        qm.dao.task_started(task["id"])
-        try:
-            process_count = tm.process_interval(
-                args.instrument,
-                start_time=astropy.time.Time(task["start_time"].isoformat(), format="isot", scale="utc"),
-                end_time=astropy.time.Time(task["end_time"].isoformat(), format="isot", scale="utc"),
-            )
+    # check for failed tasks and add them to the processing queue again
+    failed_tasks = qm.failed_tasks(args.repo, max_retries=3)
 
-            count_exposures += process_count["exposures"]
-            count_visits1 += process_count["visits1"]
+    tasks += failed_tasks
 
-            qm.dao.task_update_counts(
-                task["id"],
-                exposures=process_count["exposures"],
-                visits1=process_count["visits1"],
-            )
+    return tasks
 
-            qm.dao.task_completed(task["id"])
 
-        except Exception as e:
-            log.error(f"Error processing task: {e}")
-            qm.dao.task_failed(task["id"], error=str(e))
-        count_task += 1
+async def process_tasks(
+    tasks,
+    qm: QueueManager,
+    tm: Transform,
+    log: logging.Logger,
+    instrument: str,
+    timewindow: int,
+    batch_size: int = 50,
+):
+    """Process all tasks.
 
-    # Check if there are more tasks to run
-    # If not, create new tasks
-    if start_time is None and end_time is None:
-        tasks = qm.recent_tasks_to_run()
-        if len(tasks) == 0:
-            log.info("No more tasks to run.")
-            qm.create_tasks(
-                process_interval=int(args.timedelta),
-                time_window=int(args.timewindow),
-            )
+    Args:
+        tasks: List of tasks to process.
+        qm (QueueManager): The queue manager instance.
+        tm (Transform): The transform instance.
+        log (logging.Logger): The logger instance.
+        instrument (str): The instrument name.
+        timewindow (int): The time window value.
+    """
+    total_tasks, total_exposures, total_visits1 = 0, 0, 0
+    num_batches = (len(tasks) + batch_size - 1) // batch_size  # Calculate total number of batches
 
+    for batch_idx in range(num_batches):
+        batch = tasks[batch_idx * batch_size : (batch_idx + 1) * batch_size]
+        log.info(f"Processing batch {batch_idx + 1}/{num_batches} with {len(batch)} tasks.")
+
+        for task in batch:
+            counts = _process_task(task, qm, tm, log, instrument, timewindow, retry=task.get("retry") is True)
+            total_exposures += counts["exposures"]
+            total_visits1 += counts["visits1"]
+            total_tasks += 1
+
+    # Log summary
     log.info("======================================")
-    log.info(f"Total Tasks: {count_task}")
-    log.info(f"Processed Exposures: {count_exposures} (including overlaps)")
-    log.info(f"Processed Visits1: {count_visits1} (including overlaps)")
-    t1 = datetime.now()
-    dt = t1 - t0
-    log.info(f"Total time: {dt}")
-    log.info("End of processing")
+    log.info(f"Total Tasks Processed: {total_tasks}")
+    log.info(f"Total Exposures: {total_exposures}")
+    log.info(f"Total Visits1: {total_visits1}")
+
+
+async def main() -> None:
+    """Entry point of the program."""
+    t0 = datetime.now()
+    parser = build_argparser()
+    args = parser.parse_args()
+
+    log = get_logger(args.logfile)
+
+    # Instantiate the butler
+    butler = Butler(args.repo)
+    log.debug(f"Butler instantiated: {butler}")
+
+    # Instantiate the EFD
+    efd = InfluxDbDao(args.efd_conn_str)
+    log.debug(f"EFD connection initialized: {efd}")
+
+    # Read configuration
+    config = read_config(args.config_name)
+    log.debug(f"Configuration loaded with {len(config['columns'])} columns.")
+
+    # Instantiate the main transform class
+    tm = Transform(
+        butler=butler,
+        db_uri=args.db_conn_str,
+        efd=efd,
+        config=config,
+        logger=log,
+        commit_every=100,
+    )
+
+    # Instantiate the queue manager
+    qm = QueueManager(
+        db_uri=args.db_conn_str,
+        schema=tm.get_schema_by_instrument(args.instrument),
+        logger=log,
+    )
+
+    # Convert start_time and end_time to Astropy Time objects
+    start_time = _to_astropy_time(datetime.fromisoformat(args.start_time)) if args.start_time else None
+    end_time = _to_astropy_time(datetime.fromisoformat(args.end_time)) if args.end_time else None
+
+    # Handle workflows based on mode
+    if args.mode == "job":
+        if not start_time or not end_time:
+            log.error("Start time and end time are required in 'job' mode.")
+            sys.exit(1)
+        tasks = await handle_job(qm, log, args, start_time, end_time)
+    elif args.mode == "cronjob":
+        tasks = await handle_cronjob(qm, log, args)
+
+    # Process tasks
+    await process_tasks(tasks, qm, tm, log, args.instrument, int(args.timewindow), batch_size=50)
+
+    log.info(f"Elapsed Time: {datetime.now() - t0}")
     sys.exit(0)
 
 
 if __name__ == "__main__":
-
-    # Execution example
-    # python python/lsst/consdb/transform_efd.py \
-    #     -i LATISS \
-    #     -s 2024-04-25T00:00:00  \
-    #     -e 2024-04-30T23:59:59 \
-    #     -r /repo/embargo \
-    #     -d sqlite:///$PWD/.tmp/test.db \
-    #     -E usdf_efd \
-    #     -c $PWD/.tmp/config.yaml \
-    #     -l $PWD/.tmp/transform.log
     asyncio.run(main())
